ed9b3ed5733e5d5cad514effbab7088a3dc6ce98
Kevin Alexander Ramírez Mushín <kevinramirez202231207@cunoc.edu.gt>
2024-09-08 09:37:49 -0600

    se creo la clase de opciones usuario

---
+++ CodigoFuente/Proyecto_UnoIPC2/src/main/java/com/mycompany/proyecto_unoipc2/backend/Utileria/OpcionesUsuario.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyecto_unoipc2.backend.Utileria;
+
+/**
+ *
+ * @author kevin-mushin
+ */
+public class OpcionesUsuario {
+    
+    private String opcionNombre;
+    private String opcionAccion;
+
+    public OpcionesUsuario() {
+    }
+
+    public String getOpcionNombre() {
+        return opcionNombre;
+    }
+
+    public void setOpcionNombre(String opcionNombre) {
+        this.opcionNombre = opcionNombre;
+    }
+
+    public String getOpcionAccion() {
+        return opcionAccion;
+    }
+
+    public void setOpcionAccion(String opcionAccion) {
+        this.opcionAccion = opcionAccion;
+    }
+    
+    
+    
+}
